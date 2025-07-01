ec7ad38d96af4ea8611e161e327ce49eb6f23608
Markus-Scl <76035547+Markus-Scl@users.noreply.github.com>
2025-07-01 15:25:40 +0200

    added top nav bar

--- client/src/components/OutletWrapper.tsx
+++ client/src/components/OutletWrapper.tsx
@@ -1,8 +1,14 @@
 import type {FC, ReactNode} from 'react';
+import {TopToolBar} from './TopToolBar';
 
 interface OutletWrapperProps {
 	children: ReactNode;
 }
 export const OutletWrapper: FC<OutletWrapperProps> = ({children}) => {
-	return <div className="h-dvh w-dvh content-center">{children}</div>;
+	return (
+		<div className="h-dvh w-full bg-gradient-to-br from-blue-50 to-indigo-100">
+			<TopToolBar />
+			{children}
+		</div>
+	);
 };
