ee719fa9ebd5616c0ce6d8e25e84787156584197
Markus-Scl <76035547+Markus-Scl@users.noreply.github.com>
2025-07-23 10:58:14 +0200

    wp dockerize app

--- server/main.go
+++ server/main.go
@@ -13,35 +13,39 @@ import (
 	"github.com/joho/godotenv"
 )
 
-
 func main() {
+	// Try to load .env file, but don't fail if it doesn't exist
+	// This allows the app to work with environment variables from docker-compose
 	err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-    }
+	if err != nil {
+		log.Println("Warning: .env file not found, using environment variables")
+	}
 
 	user := os.Getenv("DB_USER")
-    password := os.Getenv("DB_PASSWORD")
-    dbname := os.Getenv("DB_NAME")
-    host := os.Getenv("DB_HOST")
-    dbPort := os.Getenv("DB_PORT")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	host := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
 
+	// Validate required environment variables
+	if user == "" || password == "" || dbname == "" || host == "" || dbPort == "" {
+		log.Fatal("Missing required database environment variables")
+	}
 
-    dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, dbPort, dbname)
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", user, password, host, dbPort, dbname)
 
-    db.InitDB(dsn)
+	db.InitDB(dsn)
 
 	mux := router.NewMiddlewareMux(middleware.CORS(), middleware.Logging(), middleware.Auth())
 
 	mux.Handle("OPTIONS /", func(w http.ResponseWriter, r *http.Request) {
-         w.WriteHeader(http.StatusOK)
-    })
+		w.WriteHeader(http.StatusOK)
+	})
 
 	fileServer := http.FileServer(http.Dir("./"))
 	mux.Handle("GET /uploads/products/", func(w http.ResponseWriter, r *http.Request) {
 		http.StripPrefix("/", fileServer).ServeHTTP(w, r)
 	})
-
 
 	//User handling
 	mux.Handle("POST /register", routeHandler.AddUser)
@@ -61,8 +65,11 @@ func main() {
 	//Product statuses
 	mux.Handle("GET /productstatus", routeHandler.GetProductStatuses)
 
-
-	serverPort:= ":" + os.Getenv("SERVER_PORT")
-    log.Println("Server listening on", serverPort)
-    log.Fatal(http.ListenAndServe(serverPort, mux))
+	serverPort := ":" + os.Getenv("SERVER_PORT")
+	if os.Getenv("SERVER_PORT") == "" {
+		serverPort = ":8080" // Default port
+	}
+	
+	log.Println("Server listening on", serverPort)
+	log.Fatal(http.ListenAndServe(serverPort, mux))
 }
